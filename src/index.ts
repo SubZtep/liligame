1438434d2ced02dafc77228b95e183dc7e4ff190
Andras Serfozo <subztep@gmail.com>
2023-03-11 02:14:04 +0000

    Separate socket handling

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,8 @@
-import { parseMessage } from "./lib/socket"
-import type { PlayerSession, Axes } from "./types/ws"
 import "./components/joy-stick"
 import "./styles/index.css"
-import { v4 } from "uuid"
+import "./lib/socket"
 
 // Set current time day/night gradient
 document.body.classList.add(`sky-gradient-${new Date().getHours()}`)
-
-const uuid = v4()
-
-document.querySelector("h1")!.innerHTML = uuid
-const playerEl = document.getElementById("player")!
-const debugEl = document.getElementById("debug")!
-
-const socket = new WebSocket(import.meta.env.VITE_WS_URL)
-
-const rawPlayer: PlayerSession = {}
-const player = new Proxy(rawPlayer, {
-  set(obj, prop, value) {
-    obj[prop] = value
-
-    const packet = JSON.stringify({ uuid, position: value })
-    socket.send(packet)
-
-    playerEl.innerHTML = JSON.stringify(rawPlayer, null, 2)
-    return true
-  }
-})
-
-const sessions = new Map<string, PlayerSession>()
-
-socket.addEventListener("message", ({ data }) => {
-  const msg = parseMessage(data)
-  if (!msg.uuid) throw new Error("Invalid message, missing uuid")
-
-  const { uuid: msgUuid, position } = msg
-  sessions.set(msgUuid, { position })
-
-  debugEl.innerHTML = JSON.stringify(Array.from(sessions.entries()), null, 2)
-})
-
-document.querySelector("joy-stick")!.addEventListener("PlayerPositionChanged", ev => {
-  player.position = (ev as CustomEvent<Axes>).detail
-})
 
 export {}
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,42 @@
-import type { SocketMessage } from "../types/ws"
+import { v4 } from "uuid"
+import type { SocketMessage, PlayerSession, Axes } from "../types/ws"
+
+export const uuid = v4()
+
+document.querySelector("h1")!.innerHTML = uuid
+const playerEl = document.getElementById("player")!
+const debugEl = document.getElementById("debug")!
+
+const socket = new WebSocket(import.meta.env.VITE_WS_URL)
+
+const rawPlayer: PlayerSession = {}
+const player = new Proxy(rawPlayer, {
+  set(obj, prop, value) {
+    obj[prop] = value
+
+    const packet = JSON.stringify({ uuid, position: value })
+    socket.send(packet)
+
+    playerEl.innerHTML = JSON.stringify(rawPlayer, null, 2)
+    return true
+  }
+})
+
+const sessions = new Map<string, PlayerSession>()
+
+socket.addEventListener("message", ({ data }) => {
+  const msg = parseMessage(data)
+  if (!msg.uuid) throw new Error("Invalid message, missing uuid")
+
+  const { uuid: msgUuid, position } = msg
+  sessions.set(msgUuid, { position })
+
+  debugEl.innerHTML = JSON.stringify(Array.from(sessions.entries()), null, 2)
+})
+
+document.querySelector("joy-stick")!.addEventListener("PlayerPositionChanged", ev => {
+  player.position = (ev as CustomEvent<Axes>).detail
+})
 
 export function parseMessage(json: string): SocketMessage {
   let msg: SocketMessage
