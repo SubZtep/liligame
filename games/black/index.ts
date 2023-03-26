172e9bb85be943092cfb1880987e50ba27ffc9e7
Andras Serfozo <subztep@gmail.com>
2023-03-26 20:54:04 +0100

    hello black

--- games/black/index.ts
+++ games/black/index.ts
@@ -1,14 +1,5 @@
-import "~/components/canvas-confetti"
+// import "~/components/wc-debug"
 import "~/styles/index.css"
 import "./index.css"
-
-const confetti = document.querySelector("canvas-confetti")
-
-document.body.addEventListener("click", ({ clientX, clientY }) => {
-  const { innerWidth, innerHeight } = window
-  const normalAxes = { x: clientX / innerWidth, y: clientY / innerHeight }
-
-  confetti?.setAttribute("origin", JSON.stringify(normalAxes))
-})
 
 console.log("Hello World")
