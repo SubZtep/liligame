cff76b5e2d8c1318b97e23c64fc72829a5d44429
Andras Serfozo <subztep@gmail.com>
2023-05-09 21:08:46 +0100

    Display cursor over websocket

--- src/app/touches.ts
+++ src/app/touches.ts
@@ -1,37 +1,40 @@
-export function onTouch(start: (x: number, y: number) => HTMLElement, end: (el: HTMLElement) => void) {
-  let el: HTMLElement
-
-  const moveEl = (x: number, y: number) => {
-    el.setAttribute("cx", x.toString())
-    el.setAttribute("cy", y.toString())
-    // document.body.style.setProperty("--cursor-pos", `translate(${ev.clientX}px, ${ev.clientY}px)`)
-  }
-
+export function onStart(cb: (x: number, y: number) => void) {
   document.body.addEventListener("touchstart", ev => {
-    const { clientX, clientY } = ev.touches[0]
-    el = start(clientX, clientY)
-  })
-  document.body.addEventListener("touchmove", ev => {
     const { clientX: x, clientY: y } = ev.touches[0]
-    el.style.setProperty("--pos", `translate(${x}px, ${y}px)`)
-  })
-  document.body.addEventListener("touchend", _ev => {
-    // const { clientX, clientY } = ev.touches[0]
-    end(el)
+    cb(x, y)
   })
   document.body.addEventListener("mousedown", ev => {
-    const { clientX, clientY } = ev
-    el = start(clientX, clientY)
-    // setTimeout(() => end(el), 150)
+    if (ev.button === 0) {
+      const { clientX: x, clientY: y } = ev
+      cb(x, y)
+    }
+  })
+}
+
+export function onMove(cb: (x: number, y: number) => void) {
+  document.body.addEventListener("touchmove", ev => {
+    if (ev.touches.length > 1) {
+      const { clientX: x, clientY: y } = ev.touches[0]
+      cb(x, y)
+    }
   })
   document.body.addEventListener("mousemove", ev => {
-    const { clientX: x, clientY: y } = ev
-    el.style.setProperty("--pos", `translate(${x}px, ${y}px)`)
+    if (ev.button === 0) {
+      const { clientX: x, clientY: y } = ev
+      cb(x, y)
+    }
   })
-  document.body.addEventListener("mouseup", _ev => {
-    end(el)
-    // const { clientX, clientY } = ev
-    // el = start(clientX, clientY)
-    // setTimeout(() => end(el), 150)
+}
+
+export function onEnd(cb: Fn) {
+  document.body.addEventListener("touchend", ev => {
+    const { clientX: x, clientY: y } = ev.touches[0]
+    cb(x, y)
+  })
+  document.body.addEventListener("mouseup", ev => {
+    if (ev.button === 0) {
+      const { clientX: x, clientY: y } = ev
+      cb(x, y)
+    }
   })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,86 @@
 import { player } from "./app/player"
-import { onTouch } from "./app/touches"
+import { onStart, onMove, onEnd } from "./app/touches"
 import "./style.css"
 
 const socket = new WebSocket(import.meta.env.VITE_WSPP)
 
-onTouch(
-  (x, y) => {
-    const msg = {
-      id: player.id,
-      color: player.color,
-      x: x / window.innerWidth,
-      y: y / window.innerHeight
-    }
-    socket.send(JSON.stringify(msg))
+const elements = new Map<string, HTMLElement>()
+const { id, color } = player
+let el: HTMLElement
 
-    // const el = createSVGCircle(x, y, color)
-    const el = document.createElement("div")
-    el.style.setProperty("--pos", `translate(${x}px, ${y}px)`)
-    el.style.setProperty("--color", player.color)
-    el.classList.add("touch")
-    return document.body.appendChild(el)
-  },
-  el => {
-    document.body.removeChild(el)
+type Message =
+  | { cmd: "add"; id: string; color: string; x: number; y: number }
+  | { cmd: "move"; id: string; x: number; y: number }
+  | { cmd: "remove"; id: string }
+
+onStart((x, y) => {
+  socket.send(JSON.stringify({ cmd: "add", id, color, x, y } as Message))
+})
+
+onMove((x, y) => {
+  socket.send(JSON.stringify({ cmd: "move", id, x, y } as Message))
+})
+
+onEnd((x, y) => {
+  socket.send(JSON.stringify({ cmd: "remove", id } as Message))
+})
+
+// onTouch(
+//   (x, y) => {
+//     const msg: Message = {
+//       cmd: "add",
+//       id: player.id,
+//       color: player.color,
+//       x: x / window.innerWidth,
+//       y: y / window.innerHeight
+//     }
+//     socket.send(JSON.stringify(msg))
+
+//     // // const el = createSVGCircle(x, y, color)
+//     // const el = document.createElement("div")
+//     // el.style.setProperty("--pos", `translate(${x}px, ${y}px)`)
+//     // el.style.setProperty("--color", player.color)
+//     // el.classList.add("touch")
+//     // return document.body.appendChild(el)
+//   },
+//   // el => {
+//   //   document.body.removeChild(el)
+//   // }
+// )
+
+// socket.addEventListener("open", ev => {
+//   console.log("Connected", ev)
+// })
+
+socket.addEventListener("message", data => {
+  const msg = JSON.parse(data.data) as Message
+  let el: HTMLElement
+  switch (msg.cmd) {
+    case "add":
+      el = document.createElement("div")
+      // el.dataset.id = msg.id
+      el.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
+      el.style.setProperty("--color", msg.color)
+      el.classList.add("touch")
+      elements.set(msg.id, el)
+      document.body.appendChild(el)
+      break
+    case "move":
+      el = elements.get(msg.id)!
+      el.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
+      break
+    case "remove":
+      el = elements.get(msg.id)!
+      document.body.removeChild(el)
+      elements.delete(msg.id)
+      break
   }
-)
 
-socket.addEventListener("open", () => {
-  console.log("Connected")
+  // console.log("Message", data)
 })
 
-socket.addEventListener("message", ev => {
-  console.log("Message", ev)
-})
+// socket.addEventListener("close", ev => {
+//   console.log("Disconnected", ev)
+// })
 
 export {}
