14dcd343bbf8692497246903e59ba8c35afda01a
Andras Serfozo <subztep@gmail.com>
2023-05-09 21:23:02 +0100

    No element Map

--- src/index.ts
+++ src/index.ts
@@ -3,82 +3,55 @@ import { onStart, onMove, onEnd } from "./app/touches"
 import "./style.css"
 
 const socket = new WebSocket(import.meta.env.VITE_WSPP)
-
-const elements = new Map<string, HTMLElement>()
 const { id, color } = player
-let el: HTMLElement
 
 type Message =
   | { cmd: "add"; id: string; color: string; x: number; y: number }
   | { cmd: "move"; id: string; x: number; y: number }
   | { cmd: "remove"; id: string }
 
+const sendMessage = (msg: Message) => {
+  socket.send(JSON.stringify(msg))
+}
+
 onStart((x, y) => {
-  socket.send(JSON.stringify({ cmd: "add", id, color, x, y } as Message))
+  sendMessage({ cmd: "add", id, color, x, y })
 })
 
 onMove((x, y) => {
-  socket.send(JSON.stringify({ cmd: "move", id, x, y } as Message))
+  sendMessage({ cmd: "move", id, x, y })
 })
 
 onEnd((x, y) => {
-  socket.send(JSON.stringify({ cmd: "remove", id } as Message))
+  sendMessage({ cmd: "remove", id })
 })
 
-// onTouch(
-//   (x, y) => {
-//     const msg: Message = {
-//       cmd: "add",
-//       id: player.id,
-//       color: player.color,
-//       x: x / window.innerWidth,
-//       y: y / window.innerHeight
-//     }
-//     socket.send(JSON.stringify(msg))
-
-//     // // const el = createSVGCircle(x, y, color)
-//     // const el = document.createElement("div")
-//     // el.style.setProperty("--pos", `translate(${x}px, ${y}px)`)
-//     // el.style.setProperty("--color", player.color)
-//     // el.classList.add("touch")
-//     // return document.body.appendChild(el)
-//   },
-//   // el => {
-//   //   document.body.removeChild(el)
-//   // }
-// )
+socket.addEventListener("message", data => {
+  const msg = JSON.parse(data.data) as Message
+  let el: HTMLElement | null
+  switch (msg.cmd) {
+    case "add":
+      el = document.createElement("div")
+      el.dataset.id = msg.id
+      el.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
+      el.style.setProperty("--color", msg.color)
+      el.classList.add("touch")
+      document.body.appendChild(el)
+      break
+    case "move":
+      el = document.querySelector(`[data-id="${msg.id}"]`)
+      el?.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
+      break
+    case "remove":
+      el = document.querySelector(`[data-id="${msg.id}"]`)
+      el && document.body.removeChild(el)
+      break
+  }
+})
 
 // socket.addEventListener("open", ev => {
 //   console.log("Connected", ev)
 // })
-
-socket.addEventListener("message", data => {
-  const msg = JSON.parse(data.data) as Message
-  let el: HTMLElement
-  switch (msg.cmd) {
-    case "add":
-      el = document.createElement("div")
-      // el.dataset.id = msg.id
-      el.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
-      el.style.setProperty("--color", msg.color)
-      el.classList.add("touch")
-      elements.set(msg.id, el)
-      document.body.appendChild(el)
-      break
-    case "move":
-      el = elements.get(msg.id)!
-      el.style.setProperty("--pos", `translate(${msg.x}px, ${msg.y}px)`)
-      break
-    case "remove":
-      el = elements.get(msg.id)!
-      document.body.removeChild(el)
-      elements.delete(msg.id)
-      break
-  }
-
-  // console.log("Message", data)
-})
-
 // socket.addEventListener("close", ev => {
 //   console.log("Disconnected", ev)
 // })
