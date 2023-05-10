639fcf060bef34bbf55f2073b628a20f24898f91
Andras Serfozo <subztep@gmail.com>
2023-05-10 01:39:36 +0100

    Better redraw

--- src/index.ts
+++ src/index.ts
@@ -29,38 +29,34 @@ onEnd(() => {
 socket.addEventListener("message", data => {
   const msg = JSON.parse(data.data) as Message
   switch (msg.cmd) {
-    case "add":
-      for (let i = 0; i < 10; i++) {
-        const el = document.createElement("div")
-        el.dataset.id = msg.id
-        el.style.setProperty(
-          "--pos",
-          msg.poses[i] ? `translate(${msg.poses[i].x}px, ${msg.poses[i].y}px)` : "translate(-200px, -200px)"
-        )
-        el.style.setProperty("--color", msg.color)
-        el.classList.add("touch")
-        document.body.appendChild(el)
-      }
-      break
+    // case "add":
+    //   for (let i = 0; i < 10; i++) {
+    //     const el = document.createElement("div")
+    //     el.dataset.id = msg.id
+    //     el.style.setProperty(
+    //       "--pos",
+    //       msg.poses[i] ? `translate(${msg.poses[i].x}px, ${msg.poses[i].y}px)` : "translate(-200px, -200px)"
+    //     )
+    //     el.style.setProperty("--color", msg.color)
+    //     el.classList.add("touch")
+    //     document.body.appendChild(el)
+    //   }
+    //   break
     case "move":
       let els = document.querySelectorAll<HTMLElement>(`[data-id="${msg.id}"]`)
       if (els.length === 0) {
         for (let i = 0; i < 10; i++) {
           const el = document.createElement("div")
           el.dataset.id = msg.id
+          el.style.setProperty("--color", msg.color)
           el.classList.add("touch")
           document.body.appendChild(el)
         }
         els = document.querySelectorAll(`[data-id="${msg.id}"]`)
       }
       els.forEach((el, i) => {
-        el.style.display = "none"
-        el.style.setProperty(
-          "--pos",
-          msg.poses[i] ? `translate(${msg.poses[i].x}px, ${msg.poses[i].y}px)` : "translate(-200px, -200px)"
-        )
-        el.style.setProperty("--color", msg.color)
-        el.style.display = "block"
+        el.style.setProperty("--x", msg.poses[i] ? `${msg.poses[i].x}px` : "-200px")
+        el.style.setProperty("--y", msg.poses[i] ? `${msg.poses[i].y}px` : "-200px")
       })
       break
     case "remove":
